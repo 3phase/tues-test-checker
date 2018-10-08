163cb4e2a928e8c8df435e87f876d96ef99ad0e2
3phase <pbozhinow@gmail.com>
2018-10-08 21:16:46 +0300

    Adding basic pdf support

--- script.py
+++ script.py
@@ -3,6 +3,33 @@ import sys
 import zipfile
 import requests
 import json
+from fpdf import FPDF
+
+def string_bases(string):
+    if 'character encoding' in string:
+        return 'encoding'
+    elif 'without seeing a doctype':
+        return 'no_doctype'
+    elif 'required instance of child element "title"':
+        return 'no_title'
+    else:
+        return 'unrecognized'
+
+def publish_err(shorthand, line = -1):
+    if shorthand == 'encoding':
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_xy(0, 0)
+        pdf.set_font('arial', 'B', 13.0)
+        pdf.cell(ln=0, h=5.0, align='L', w=0, txt="Nqma posochen encoding", border=0)
+        pdf.output('test.pdf', 'F')
+    # elif shorthand == 'no_doctype':
+    #
+    # elif shorthand == 'no_title':
+    #
+    # elif shorthand == 'unrecognized':
+
+
 
 # BASE_FOLDER = 'D:/School/TUES-Teach/WD18-19/'
 BASE_FOLDER = '/home/phased/Documents/tues-test-checker/'
@@ -37,17 +64,9 @@ for file in html_pages:
 
     print("Information about page " + file + "\n")
     for message in json_data['messages']:
-        print(message)
+        if (message['type'] == 'error'):
+            err_shortcode = string_bases(message['message'])
+            # print(message['message'])
+            publish_err(err_shortcode)
 
     print("\n")
-
-
-# path = './' + EXTRACTIONS_FOLDER +  file_name_without_extention + '/' + html_pages[0]
-# data = open(path, 'rb').read()
-#
-# request = requests.post(req_target, data=data, headers={'Content-Type':'text/html'})
-# json_data = json.loads(request.text)
-#
-# print("Information about page " + html_pages[0] + "\n")
-# for message in json_data['messages']:
-#     print(message)
