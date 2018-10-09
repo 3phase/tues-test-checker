832bcff639b4f5147cfa2d0d79260cfe052b4b47
BlackBull <pepo1229@yahoo.com>
2018-10-09 14:35:46 +0300

    Fixed bugs with output

--- script.py
+++ script.py
@@ -29,19 +29,18 @@ def file_scope_definition(file_name):
     pdf.ln(8)
 
 def publish_err(shorthand, line = -1):
-    pdf.set_font('arial', '', 11)
     if shorthand == 'encoding':
-        pdf.cell(w=10, ln=2, txt='There\'s no indicated encoding. Error message: ' + shorthand, border=0, align='L')
-        pdf.ln(8)
+        message = 'There\'s no indicated encoding.'
     elif shorthand == 'doctype':
-        pdf.cell(w=10, ln=2, txt='There\'s no doctype included. Error message: ' + shorthand, border=0, align='L')
-        pdf.ln(8)
+        message='There\'s no doctype included.'
     elif shorthand == 'no_title':
-        pdf.cell(w=10, ln=2, txt='There\'s no title in the head. Error message: ' + shorthand, border=0, align='L')
-        pdf.ln(8)
-    elif shorthand == 'unrecognized':
-        pdf.cell(w=10, ln=2, txt='An unrecognized error appeared. Error message: ' + shorthand, border=0, align='L')
-        pdf.ln(8)
+        message='There\'s no title in the head.'
+    else:
+        message='An unrecognized error appeared.'
+
+    pdf.set_font('arial', '', 11)
+    pdf.cell(w=10, ln=2, txt=message + ' Error message: ' + shorthand, border=0, align='L')
+    pdf.ln(8)
 
 def publish_raw(data):
     pdf.add_page()
@@ -57,9 +56,9 @@ def publish_raw(data):
 def string_bases(string):
     if 'character encoding' in string:
         return 'encoding'
-    elif 'without seeing a doctype':
+    elif 'without seeing a doctype' in string:
         return 'doctype'
-    elif 'required instance of child element "title"':
+    elif 'required instance of child element “title”' in string:
         return 'no_title'
     else:
         return 'unrecognized'
@@ -91,32 +90,20 @@ req_target = 'https://validator.w3.org/nu/?out=json'
 
 create_pdf_service_fields()
 
-# file_scope_definition(html_pages[0])
-# publish_err('encoding')
-# publish_err('doctype')
-# file_scope_definition(html_pages[2])
-# publish_err('unrecognized')
-# publish_err('encoding')
-# publish_err('doctype')
-# publish_raw()
-# pdf.output(file_name_without_extention + '.pdf', 'F')
-
 for file in html_pages:
+    file_scope_definition(file)
     path = './' + EXTRACTIONS_FOLDER +  file_name_without_extention + '/' + file
     data = open(path, 'rb').read()
-#
+
     request = requests.post(req_target, data=data, headers={'Content-Type':'text/html'})
     json_data = json.loads(request.text)
-#
-    file_scope_definition(file)
+
     for message in json_data['messages']:
         if (message['type'] == 'error'):
             err_shortcode = string_bases(message['message'])
-            # print(message['message'])
             publish_err(err_shortcode)
         raw_output += json.dumps(json_data)
 
 
 publish_raw(raw_output)
 pdf.output(file_name_without_extention + '.pdf', 'F')
-    # print("\n")
